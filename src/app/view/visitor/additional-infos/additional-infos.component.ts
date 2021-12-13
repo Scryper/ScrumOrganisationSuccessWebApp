592f08db1c549f0c808e86b84ed4b689ee42b370
Scryper <florian.mazzeo@gmail.com>
2021-12-13 13:12:58 +0100

    martin pour toi testai le kod

--- src/app/view/visitor/additional-infos/additional-infos.component.ts
+++ src/app/view/visitor/additional-infos/additional-infos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import {UserService} from "../../../services";
 import {SosUser} from "../../../domain/sos-user";
+import {DatePipe} from "@angular/common";
 
 @Component({
     selector: 'app-additional-infos',
@@ -16,6 +17,7 @@ export class AdditionalInfosComponent implements OnInit {
     ]
     title: string = "Additional Information";
     buttonIsPressed: boolean = false;
+    isMajor: boolean = true;
 
     form:FormGroup = this.fb.group({
         main: this.fb.group({
@@ -32,27 +34,51 @@ export class AdditionalInfosComponent implements OnInit {
     ngOnInit(): void { }
 
     sendData() {
+        let rawValues = this.form.getRawValue().main;
+        let birthdateUser: Date = rawValues.birthdate;
         let user: SosUser = {
-            firstname: this.form.getRawValue().main.firstName,
-            lastname: this.form.getRawValue().main.lastName,
+            firstname: rawValues.firstName,
+            lastname: rawValues.lastName,
             password: "abcd",
             email: "abcd",
-            role: this.typeUserNames.indexOf(this.form.getRawValue().main.userType) + 1,
-            birthdate: new Date()
+            role: this.typeUserNames.indexOf(rawValues.userType) + 1,
+            birthdate: birthdateUser
         };
 
-        this.userService.addUser(user).then(result => {
-            if(result == null) {
-                // TODO : handle
-            }
-        });
+        let date: Date = new Date();
+        let currentYear: number = new Date().getFullYear();
+        let birthYear: number = birthdateUser.getFullYear();
+        let offset: number;
+        if(date.getMonth() < birthdateUser.getMonth()) {
+            offset = -1;
+        } else if(date.getMonth() == birthdateUser.getMonth()
+                && date.getDay() < birthdateUser.getDay()) {
+            offset = -1;
+        }
+        else {
+            offset = 0;
+        }
+
+        if((currentYear - birthYear + offset) < 18) this.isMajor = false;
+
+        if(this.isMajor) {
+            this.userService.addUser(user).then(result => {
+                if(result == null) {
+                    // TODO : handle
+                }
+            });
+        }
     }
 
     autoComplete() {
+        let datePipe = new DatePipe('en-GB');
+        let date = datePipe.transform(new Date(), 'dd/MM/yyyy');
+
         this.form.setValue({
             main:{
-                lastName:"Auversack",
-                firstName:"Damien",
+                lastname:"Auversack",
+                firstname:"Damien",
+                birthdate: date,
                 userType:"Developer"
             }
         })

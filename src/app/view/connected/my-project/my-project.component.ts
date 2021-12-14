8a39c546a89fd6b8a306c6bbb89b53ec3863b387
Damien Auversack <66826942+damien-auversack@users.noreply.github.com>
2021-12-14 23:08:05 +0100

    add accordion in myProject page + use flexBox for the layout

--- src/app/view/connected/my-project/my-project.component.ts
+++ src/app/view/connected/my-project/my-project.component.ts
@@ -8,17 +8,51 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class MyProjectComponent implements OnInit {
 
+    sprintActual = {
+        name: "Sprint 4",
+        US: [
+            "US7 - Time mode",
+            "US8 - Round mode"
+        ]
+    };
+
+    oldSprint = [
+        {
+            name: "Sprint 1",
+            US: [
+                "US1 - Move players",
+                "US2 - Fight"
+            ]
+        },
+        {
+            name: "Sprint 2",
+            US: [
+                "US3 - Defend",
+                "US4 - Fly"
+            ]
+        },
+        {
+            name: "Sprint 3",
+            US: [
+                "US5 - Animation",
+                "US6 - Collision"
+            ]
+        }
+    ];
+
     nameProject: string | null = "";
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute) {}
 
     ngOnInit(): void {
         this.nameProject = this.route.snapshot.paramMap.get("nameProject");
     }
 
     buttonIsPressed: boolean = false;
+    clicked: any;
 
     toggleButtonPress(isPressed:boolean) {
         this.buttonIsPressed = isPressed;
     }
+
 }

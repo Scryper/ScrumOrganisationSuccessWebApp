ef093b74bafdbe8e4058e0d4ac8abcdb05571932
Scryper <florian.mazzeo@gmail.com>
2021-12-21 12:16:51 +0100

    maybe found the problem

--- src/app/view/connected/project-manager/project-manager.component.ts
+++ src/app/view/connected/project-manager/project-manager.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthenticationService} from "../../../services";
 import {ProjectsService} from "../../../services/projects/projects.service";
 import {UsersProjectsService} from "../../../services/developers-projects/users-projects.service";
 import {Project} from "../../../domain/project";
 import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
+import {mergeMap} from "rxjs/operators";
 
 @Component({
     selector: 'app-project-manager',
     templateUrl: './project-manager.component.html',
     styleUrls: ['../../../app.component.css', './project-manager.component.css']
 })
-export class ProjectManagerComponent implements OnInit {
+export class ProjectManagerComponent implements OnInit, OnDestroy {
     private STATUS_INACTIVE: number = 1;
     private STATUS_ACTIVE: number = 2;
+
+    private subscriptionDeveloperProjectService: Subscription | undefined;
+    private subscriptionProjectService: Subscription | undefined;
+
     isProjectAlreadyExist: boolean = false;
     buttonPressed: boolean = false;
     isProductOwner: boolean = false;
@@ -36,6 +42,11 @@ export class ProjectManagerComponent implements OnInit {
                 private projectService: ProjectsService,
                 private route: Router) { }
 
+    ngOnDestroy(): void {
+        this.subscriptionDeveloperProjectService?.unsubscribe();
+        this.subscriptionProjectService?.unsubscribe();
+    }
+
     ngOnInit(): void {
         this.authenticationService.currentUser.subscribe(user => {
             if(user != null) {
@@ -53,7 +64,7 @@ export class ProjectManagerComponent implements OnInit {
         this.buttonPressed = isPressed;
     }
 
-    private async loadProjects() {
+    private loadProjects() {
         this.activeProjects = [
             {
                 id: 0,
@@ -75,7 +86,15 @@ export class ProjectManagerComponent implements OnInit {
                 repositoryUrl: ""
             }
         ];
-        // await this.developerProjectService.getByIdDeveloper(this.idUser).then(developerProjects => {
+        this.subscriptionDeveloperProjectService =this.developerProjectService.getByIdDeveloper(this.idUser).subscribe(developerProjects => {
+            for (let i = 0 ; i < developerProjects.length ; i++) {
+                this.getProjectName(developerProjects[i].idProject);
+            }
+        });
+        // this.subscriptionDeveloperProjectService =
+        //     this.developerProjectService.getByIdDeveloper(this.idUser)
+        //         .pipe();
+        //     this.developerProjectService.getByIdDeveloper(this.idUser).subscribe(developerProjects => {
         //     for (let i = 0 ; i < developerProjects.length ; i++) {
         //         this.getProjectName(developerProjects[i].idProject);
         //     }
@@ -83,23 +102,23 @@ export class ProjectManagerComponent implements OnInit {
     }
 
     private getProjectName(idProject: number) {
-        // this.projectService.getById(idProject).then(project => {
-        //     if(project.status == this.STATUS_ACTIVE || (project.status == this.STATUS_INACTIVE && this.isProductOwner)) {
-        //         if(this.activeProjects[0].name == this.noProjectsFound) {
-        //             this.activeProjects.pop();
-        //         }
-        //         this.activeProjects.push(project);
-        //     } else {
-        //         if(this.oldProjects[0].name == this.noProjectsFound) {
-        //             this.oldProjects.pop();
-        //         }
-        //         this.oldProjects.push(project);
-        //     }
-        //     // INSERT
-        //     if(this.activeProjects.length == 0 || (this.activeProjects.length == 1 && this.activeProjects[0].name == this.noProjectsFound ) ) {
-        //         this.isActiveProjectEmpty = true;
-        //     }
-        // });
+        this.subscriptionProjectService = this.projectService.getById(idProject).subscribe(project => {
+            if(project.status == this.STATUS_ACTIVE || (project.status == this.STATUS_INACTIVE && this.isProductOwner)) {
+                if(this.activeProjects[0].name == this.noProjectsFound) {
+                    this.activeProjects.pop();
+                }
+                this.activeProjects.push(project);
+            } else {
+                if(this.oldProjects[0].name == this.noProjectsFound) {
+                    this.oldProjects.pop();
+                }
+                this.oldProjects.push(project);
+            }
+            // the active project is empty if the list is empty (should not happen)
+            // or if there is only one element in the list and it is the default project
+            this.isActiveProjectEmpty = this.activeProjects.length == 0
+                || (this.activeProjects.length == 1 && this.activeProjects[0].name == this.noProjectsFound);
+        });
     }
 
     terminate(project:Project) {
